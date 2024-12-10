95614b4b50d89f4f258d8febde2b78032d8721f3
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-12-10 11:03:48 -0700

    Changes in migration files

--- src/Database/Migrations/2024-12-10105950_Inventorydetails.php
+++ src/Database/Migrations/2024-12-10105950_Inventorydetails.php
@@ -4,30 +4,30 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class InventoryDetails extends Migration {
+class Inventorydetails extends Migration {
 
     public function up() {
-        // QuotesDetails
+        // Inventorydetails
         $this->forge->addField([
             'id' => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
             'idInventory' => ['type' => 'int', 'constraint' => 11, 'null' => true],
             'idProduct' => ['type' => 'int', 'constraint' => 11, 'null' => true],
-            'lote' => ['type' => 'varchar', 'constraint' => 512, 'null' => true],
             'description' => ['type' => 'varchar', 'constraint' => 512, 'null' => true],
             'unidad' => ['type' => 'varchar', 'constraint' => 64, 'null' => true],
             'claveProductoSAT' => ['type' => 'varchar', 'constraint' => 64, 'null' => true],
             'claveUnidadSAT' => ['type' => 'varchar', 'constraint' => 64, 'null' => true],
             'codeProduct' => ['type' => 'varchar', 'constraint' => 32, 'null' => true],
-            'cant' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'price' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'porcentTax' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'porcentIVARetenido' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'IVARetenido' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'porcentISRRetenido' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'ISRRetenido' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'tax' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'neto' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
-            'total' => ['type' => 'decimal', 'constraint' => '18,4', 'null' => true],
+            'lote' => ['type' => 'varchar', 'constraint' => 64, 'null' => true],
+            'cant' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'price' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'porcentTax' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'porcentIVARetenido' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'IVARetenido' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'porcentISRRetenido' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'ISRRetenido' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'tax' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'neto' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
+            'total' => ['type' => 'decimal', 'constraint' => 18, 'null' => true],
             'created_at' => ['type' => 'datetime', 'null' => true],
             'updated_at' => ['type' => 'datetime', 'null' => true],
             'deleted_at' => ['type' => 'datetime', 'null' => true],
@@ -39,5 +39,4 @@ class InventoryDetails extends Migration {
     public function down() {
         $this->forge->dropTable('inventorydetails', true);
     }
-
 }
