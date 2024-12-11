645c1c99b5d425a2f4906ce747c77fe0290704de
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-12-11 10:18:19 -0700

    Changes routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -45,6 +45,7 @@ $routes->group('admin', function ($routes) {
                 ,['namespace' => 'julio101290\boilerplateinventory\Controllers']
                 );
 
+                
     $routes->get('products/getAllProductsInventory/(:any)/(:any)/(:any)'
                 , 'InventoryController::getAllProductsInventory/$1/$2/$3'
                 ,['namespace' => 'julio101290\boilerplateinventory\Controllers']
